1dd819c101559d5260d28f56c26bc7368c3fa59c
Chris Padilla <cdpadilla42@gmail.com>
2020-06-26 13:44:47 -0500

    Add next and prev functionality for slides

--- exercises/59 - Slider/src/index.js
+++ exercises/59 - Slider/src/index.js
@@ -1,8 +1,8 @@
 function Slider(slider) {
   let slides = Array.from(slider.querySelectorAll('.slide'));
-  // console.log(slides);
   let current = slider.querySelector('.current') || slides[0];
-  // console.log(current);
+  let nextBttn = slider.querySelector('.goToNext');
+  let prevBttn = slider.querySelector('.goToPrev');
 
   function showCurrentSlide() {
     current.classList.add('current');
@@ -10,10 +10,25 @@ function Slider(slider) {
 
   function setPrevAndNextSlides() {
     console.log(current.nextElementSibling);
-    current.nextElementSibling.classList.add('next');
-    current.previousElementSibling
-      ? current.previousElementSibling.classList.add('prev')
-      : slides[slides.length - 1].classList.add('prev');
+    // add class to next slide
+    if (current.nextElementSibling) {
+      current.nextElementSibling.classList.add('next');
+    } else {
+      // if at the end, add to the first slide in list
+      slides[0].classList.add('next');
+    }
+    // add class to prev slide
+    if (current.previousElementSibling) {
+      current.previousElementSibling.classList.add('prev');
+    } else {
+      // if at the end, add to the last slide in list
+      slides[slides.length - 1].classList.add('prev');
+    }
+  }
+
+  function removePrevAndNextClasses() {
+    slider.querySelector('.next').classList.remove('next');
+    slider.querySelector('.prev').classList.remove('prev');
   }
 
   function initializeSlider() {
@@ -21,8 +36,31 @@ function Slider(slider) {
     setPrevAndNextSlides();
   }
 
+  function shiftCurrentSlide() {
+    slider.querySelector('.current').classList.remove('current');
+    current.classList.add('current');
+  }
+
+  function nextSlideGo() {
+    removePrevAndNextClasses();
+    // move current over one
+    current = current.nextElementSibling || slides[0];
+    shiftCurrentSlide();
+    setPrevAndNextSlides();
+  }
+
+  function prevSlideGo() {
+    removePrevAndNextClasses();
+    // move current over one
+    current = current.previousElementSibling || slides[slides.length - 1];
+    shiftCurrentSlide();
+    setPrevAndNextSlides();
+  }
+
   // Event listeners
   window.addEventListener('load', initializeSlider);
+  nextBttn.addEventListener('click', nextSlideGo);
+  prevBttn.addEventListener('click', prevSlideGo);
 }
 
 Slider(document.querySelector('.slider'));
