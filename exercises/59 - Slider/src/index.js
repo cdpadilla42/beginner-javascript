28f74e63144dbfb1017bf79f97dc39caee4acc3f
Chris Padilla <cdpadilla42@gmail.com>
2020-06-29 13:24:16 -0500

    Refactor slider for ogject constructor

--- exercises/59 - Slider/src/index.js
+++ exercises/59 - Slider/src/index.js
@@ -1,71 +1,81 @@
 function Slider(slider) {
-  if (!slider instanceof Element) {
+  this.slider = slider;
+
+  if (!this.slider instanceof Element) {
     throw new Error('No slider passed in');
   }
   // create some variables for working with the slider
-  let prev;
-  let current;
-  let next;
+  this.prev;
+  this.current;
+  this.next;
   // select elements needed for the slider
-  const slides = slider.querySelector('.slides');
-  const nextBttn = slider.querySelector('.goToNext');
-  const prevBttn = slider.querySelector('.goToPrev');
-
-  function startSlider() {
-    current = slider.querySelector('.current') || slides.firstElementChild;
-    prev = current.previousElementSibling || slides.lastElementChild;
-    next = current.nextElementSibling || slides.firstElementChild;
-  }
-
-  function applyClasses() {
-    current.classList.add('current');
-    prev.classList.add('prev');
-    next.classList.add('next');
-  }
-
-  function move(direction) {
-    // strip all the classes off the slides
-    const classesToRemove = ['prev', 'current', 'next'];
-    prev.classList.remove(...classesToRemove);
-    next.classList.remove(...classesToRemove);
-    current.classList.remove(...classesToRemove);
-
-    if (direction === 'back') {
-      // make a new array of the new values and destructure them over and into the prev current and next variables
-
-      [prev, current, next] = [
-        prev.previousElementSibling || slides.lastElementChild,
-        prev,
-        current,
-      ];
-    } else {
-      [prev, current, next] = [
-        current,
-        next,
-        next.nextElementSibling || slides.firstElementChild,
-      ];
-    }
-    applyClasses();
-  }
-
-  function handleKeyUp(e) {
-    const isActive = document.activeElement === slides;
-    if (isActive) {
-      if (e.key === 'ArrowRight') move();
-      if (e.key === 'ArrowLeft') move('back');
-    }
-  }
+  this.slides = this.slider.querySelector('.slides');
+  this.nextBttn = this.slider.querySelector('.goToNext');
+  this.prevBttn = this.slider.querySelector('.goToPrev');
 
   // constructor
   // when slider created, runs
-  startSlider();
-  applyClasses();
 
   // Event Listeners
-  prevBttn.addEventListener('click', () => move('back'));
-  nextBttn.addEventListener('click', move);
-  window.addEventListener('keyup', handleKeyUp);
+  this.prevBttn.addEventListener('click', () => this.move('back'));
+  this.nextBttn.addEventListener('click', () => this.move());
+  window.addEventListener('keyup', (e) => this.handleKeyUp(e));
+
+  this.startSlider();
+  this.applyClasses();
 }
 
-const mySlider = Slider(document.querySelector('.slider'));
-const dogSlider = Slider(document.querySelector('.dog-slider'));
+Slider.prototype.startSlider = function() {
+  this.current =
+    this.slider.querySelector('.current') || this.slides.firstElementChild;
+  this.prev =
+    this.current.previousElementSibling || this.slides.lastElementChild;
+  this.next = this.current.nextElementSibling || this.slides.firstElementChild;
+};
+
+Slider.prototype.applyClasses = function() {
+  this.current.classList.add('current');
+  this.prev.classList.add('prev');
+  this.next.classList.add('next');
+};
+
+Slider.prototype.handleKeyUp = function(e) {
+  // debugger;
+  const isActive = document.activeElement === this.slides;
+  if (isActive) {
+    if (e.key === 'ArrowRight') this.move();
+    if (e.key === 'ArrowLeft') this.move('back');
+  }
+};
+
+Slider.prototype.constructor = function() {
+  // blank
+};
+
+Slider.prototype.move = function(direction) {
+  // strip all the classes off the slides
+  this.classesToRemove = ['prev', 'current', 'next'];
+  this.prev.classList.remove(...this.classesToRemove);
+  this.next.classList.remove(...this.classesToRemove);
+  this.current.classList.remove(...this.classesToRemove);
+
+  if (direction === 'back') {
+    // make a new array of the new values and destructure them over and into the prev current and next variables
+
+    [this.prev, this.current, this.next] = [
+      this.prev.previousElementSibling || this.slides.lastElementChild,
+      this.prev,
+      this.current,
+    ];
+  } else {
+    [this.prev, this.current, this.next] = [
+      this.current,
+      this.next,
+      this.next.nextElementSibling || this.slides.firstElementChild,
+    ];
+  }
+  this.applyClasses();
+};
+
+const mySlider = new Slider(document.querySelector('.slider'));
+const dogSlider = new Slider(document.querySelector('.dog-slider'));
