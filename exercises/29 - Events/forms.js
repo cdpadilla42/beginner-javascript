7118091b466e4ccd75bde4f96cd95be356d8b091
Chris Padilla <cdpadilla42@gmail.com>
2020-07-01 13:09:21 -0500

    Basic for events

--- exercises/29 - Events/forms.js
+++ exercises/29 - Events/forms.js
@@ -1,1 +1,27 @@
-console.log('ey');
+const link = document.querySelector('.test_link');
+link.addEventListener('click', (e) => {
+  const contine = confirm('Want to head on?');
+  if (!contine) {
+    e.preventDefault();
+  }
+});
+
+const signupForm = document.querySelector('[name="signup"]');
+
+signupForm.addEventListener('submit', (e) => {
+  const name = e.currentTarget.name.value;
+  if (name.toLowerCase().includes('chad')) {
+    e.preventDefault();
+    alert('Sorry Bro');
+  }
+});
+
+function logEvent(event) {
+  console.log(event.type);
+  console.log(event.currentTarget.value);
+}
+
+console.log(signupForm.name.addEventListener('keyup', logEvent));
+console.log(signupForm.name.addEventListener('keydown', logEvent));
+console.log(signupForm.name.addEventListener('focus', logEvent));
+console.log(signupForm.name.addEventListener('blur', logEvent));
