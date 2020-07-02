47a207e20de8506cc22360781d58c536e6404a20
Chris Padilla <cdpadilla42@gmail.com>
2020-07-02 13:01:37 -0500

    Took a crack at solving tab riddle

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,34 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+function tabs(tablist) {
+  const tabs = Array.from(document.querySelectorAll('[role="tab"]'));
+  const tabPanels = Array.from(document.querySelectorAll('[role="tabpanel"]'));
+
+  function switchTabContent(id) {
+    const prevOpenTab = tabPanels.find((tab) => !tab.hidden);
+    const newOpenTab = tabPanels.find(
+      (tab) => tab.getAttribute('aria-labelledby') === id
+    );
+    prevOpenTab.hidden = true;
+    newOpenTab.hidden = false;
+  }
+
+  function handleTabClick(e) {
+    // switch active tab and remove previously active tag
+    const prevTag = tabs.find((tab) => tab.matches('[aria-selected="true"]'));
+    if (prevTag === e.currentTarget) return;
+
+    prevTag.setAttribute('aria-selected', 'false');
+    e.currentTarget.setAttribute('aria-selected', 'true');
+
+    // switch visible tag
+    switchTabContent(e.currentTarget.id);
+
+    // hide current open tag
+    // show needed tag
+  }
+
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', handleTabClick);
+  });
+}
+
+tabs(document.querySelector('[role="tablist"]'));
