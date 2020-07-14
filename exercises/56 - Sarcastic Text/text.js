ca0f6a217a6c58c18ab5a3c3424769858f6fb323
Chris Padilla <cdpadilla42@gmail.com>
2020-07-14 10:20:48 -0500

    Write sarcastic function

--- exercises/56 - Sarcastic Text/text.js
+++ exercises/56 - Sarcastic Text/text.js
@@ -20,6 +20,18 @@ function textGenerator(typer) {
     }
   }
 
+  function makeSarcastic() {
+    const input = typer.querySelector('[name="text"]').value;
+    const sarcastic = input
+      .split('')
+      .map((letter, i) => {
+        if (i % 2) return letter.toUpperCase();
+        return letter;
+      })
+      .join('');
+    return sarcastic;
+  }
+
   textOptionInputs.forEach((input) =>
     input.addEventListener('input', (e) => {
       generateText(e.currentTarget);
@@ -28,6 +40,7 @@ function textGenerator(typer) {
 
   // generate text for currently selected option
   generateText(textOptionInputs.find((input) => input.checked));
+  makeSarcastic();
 }
 
 textGenerator(document.querySelector('.typer'));
